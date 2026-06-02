5e7625082a739467318bf128ed31c738e61baf86
Pritesh Alshetty <alshettypritesh4@gmail.com>
2026-06-02 12:22:40 +0530

    updated query of homescreen getdata to use latest row

--- controllers/homescreen.controller.js
+++ controllers/homescreen.controller.js
@@ -3,7 +3,7 @@ const db = require("../config/db.js");
 async function getHomescreenData(params) {
     {
         try {
-            const [result] = await db.query("SELECT *, DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') AS created_at_ist FROM homescreen_data LIMIT 1");
+            const [result] = await db.query("SELECT *, DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') AS created_at_ist FROM homescreen_data ORDER BY created_at DESC LIMIT 1");
             if (result.length > 0) {
                 return result[0];
             } else {
