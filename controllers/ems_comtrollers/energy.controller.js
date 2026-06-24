4fab715af83cb01024e6a5c06d262fa05e0efa58
priteshAlshetty <alshettypritesh4@gmail.com>
2026-06-24 16:12:24 +0530

    added energy calculation route

---
+++ controllers/ems_comtrollers/energy.controller.js
@@ -0,0 +1,40 @@
+const db = require("../../config/mfmDB.js");
+
+async function getEnergyData(params) {
+
+    const sql = `WITH hourly_data AS (
+    SELECT
+        DATE_FORMAT(date_time, '%Y-%m-%d %H:00:00') AS hour_time,
+        KWh,
+        current_avg,
+        current_i1,
+        current_i2,
+        current_i3,
+        ROW_NUMBER() OVER (
+            PARTITION BY DATE_FORMAT(date_time, '%Y-%m-%d %H')
+            ORDER BY date_time DESC
+        ) AS rn
+    FROM ${params.meter_name}
+    WHERE date_time >= ?
+    AND date_time <  ?
+)
+
+SELECT
+    hour_time,
+    ROUND(KWh,2) AS kwh,
+    ROUND(current_avg,2) AS current_avg,
+    ROUND(current_i1,2) AS current_i1,
+    ROUND(current_i2,2) AS current_i2,
+    ROUND(current_i3,2) AS current_i3
+FROM hourly_data
+WHERE rn = 1
+ORDER BY hour_time;`;
+
+    const [rows] = await db.query(sql, [params.from, params.to]);
+    return rows;
+
+}
+
+module.exports = {
+    getEnergyData
+};
--- routes/mfmRoutes.js
+++ routes/mfmRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getNames, getMFMData } = require("../controllers/ems_comtrollers/rawdata.controller.js");
-
+const { getEnergyData } = require("../controllers/ems_comtrollers/energy.controller.js");
 
 router.get("/meterNames", async (req, res) => {
     try {
@@ -59,6 +59,39 @@ router.get("/meterData", async (req, res) => {
             error: err.message,
             stack: err.stack,
             location: 'At api call /ems/rawdata/meterData => catch block'
+        });
+    }
+});
+
+router.get("/energyData", async (req, res) => {
+    try {
+        const meter_name = req.query.meterName;
+        const from = req.query.from;
+        const to = req.query.to;
+        const params = {
+            meter_name,
+            from,
+            to
+        };
+        const result = await getEnergyData(params);
+        if (result) {
+            res.status(200).json({
+                success: true,
+                data: result
+            });
+        } else {
+            res.status(400).json({
+                success: false,
+                Msg: `Data not found for meter: ${params.meter_name} and date range: ${params.from} to ${params.to}.`
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            errMsg: 'Internal server error',
+            success: false,
+            error: err.message,
+            stack: err.stack,
+            location: 'At api call /ems/rawdata/energyData => catch block'
         });
     }
 });
